05a783dd8406fdab47c59ae3a35b34da17fe7434
Elliot Sayes <elliot@sayes.cloud>
2024-01-23 15:32:43 +0800

    Export expoDevPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-export { createInspector } from './createInspector';
-export { createWebSocketInspector, createWebSocketReceiver } from './webSocket';
-export { createBrowserInspector, createBrowserReceiver } from './browser';
+export { createInspector } from "./createInspector";
+export { createWebSocketInspector, createWebSocketReceiver } from "./webSocket";
+export { createBrowserInspector, createBrowserReceiver } from "./browser";
+export {
+  createExpoDevPluginInspector,
+  forwardExpoDevToolsToInspector,
+} from "./expoDevPlugin";
 export type {
   StatelyActorEvent,
   StatelyInspectionEvent,
   StatelyEventEvent,
   StatelySnapshotEvent,
-} from './types';
+} from "./types";
